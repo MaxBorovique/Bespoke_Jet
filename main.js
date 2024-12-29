868bbe5a2ec43d732e1ec03ee25efefd422d8595
Maks <max.borovique@gmail.com>
2024-12-29 21:28:26 +0200

    add more animations

--- main.js
+++ main.js
@@ -23,12 +23,12 @@ const sliderConfig = {
     },
   ],
   selectors: {
-    image: '.slider__image',
-    text: '.slider__text',
+    image: ".slider__image",
+    text: ".slider__text-content",
     counter: ".slider__navigation-page",
-    prevButton: '.slider__navigation-prev',
-    nextButton: '.slider__navigation-next',
-    bannerButton: '.banner__button',
+    prevButton: ".slider__navigation-prev",
+    nextButton: ".slider__navigation-next",
+    bannerButton: ".banner__button",
   },
 };
 // #endregion
@@ -41,7 +41,7 @@ const elements = {
   prevButton: document.querySelector(sliderConfig.selectors.prevButton),
   nextButton: document.querySelector(sliderConfig.selectors.nextButton),
   bannerButton: document.querySelector(sliderConfig.selectors.bannerButton),
-}
+};
 // #endregion
 
 // #region: Slider state and constants
@@ -56,181 +56,199 @@ const SLIDER_LENGTH = sliderConfig.slides.length;
 // #endregion
 
 // #region: Functins
-  const updateSlider = () => {
-    const currentSlide = sliderConfig.slides[sliderState.currentSlideIndex];
+const updateSlider = () => {
+  const currentSlide = sliderConfig.slides[sliderState.currentSlideIndex];
 
-    if(elements.image) {
-      elements.image.src = currentSlide.src;
-    }
-
-    if(elements.text) {
-      elements.text.textContent = currentSlide.text;
-    }
-
-    if(elements.counter) {
-      elements.counter.textContent = `${sliderState.currentSlideIndex + 1} / ${SLIDER_LENGTH}`;
-    }
-  };
-
-  const nextSlide = () => {
-    sliderState.currentSlideIndex = (sliderState.currentSlideIndex + 1) % SLIDER_LENGTH; 
-    updateSlider();
-
-
-  };
-
-  const prevSlide = () => {
-    sliderState.currentSlideIndex = (sliderState.currentSlideIndex - 1 + SLIDER_LENGTH ) % SLIDER_LENGTH;
-    updateSlider();
-  };
-
-  const autoPlayStart = (delay = 5000) => {
-    autoPlayStop();
-    sliderState.interval = setInterval(nextSlide, delay);
-  };
-
-  const autoPlayStop = () => {
-    if(sliderState.interval) {
-      clearInterval(sliderState.interval);
-      sliderState.interval = null;
-    }
-  };
-
-  const sliderNavigation = () => {
-    if(elements.prevButton) {
-      elements.prevButton.addEventListener('click', () => {
-        autoPlayStop();
-        prevSlide();
-      })
-    }
-
-    if(elements.nextButton) {
-      elements.nextButton.addEventListener('click', () => {
-        autoPlayStop();
-        nextSlide();
-      })
-    }
+  if (elements.image) {
+    elements.image.src = currentSlide.src;
   }
 
-  const initializeSlider = () => {
-    updateSlider();
-    sliderNavigation();
-    autoPlayStart();
-  };
+  if (elements.text) {
+    elements.text.textContent = currentSlide.text;
+  }
 
-  initializeSlider();
+  if (elements.counter) {
+    elements.counter.textContent = `${
+      sliderState.currentSlideIndex + 1
+    } / ${SLIDER_LENGTH}`;
+  }
+};
+
+const nextSlide = () => {
+  sliderState.currentSlideIndex =
+    (sliderState.currentSlideIndex + 1) % SLIDER_LENGTH;
+  updateSlider();
+};
+
+const prevSlide = () => {
+  sliderState.currentSlideIndex =
+    (sliderState.currentSlideIndex - 1 + SLIDER_LENGTH) % SLIDER_LENGTH;
+  updateSlider();
+};
+
+const autoPlayStart = (delay = 5000) => {
+  autoPlayStop();
+  sliderState.interval = setInterval(nextSlide, delay);
+};
+
+const autoPlayStop = () => {
+  if (sliderState.interval) {
+    clearInterval(sliderState.interval);
+    sliderState.interval = null;
+  }
+};
+
+const sliderNavigation = () => {
+  if (elements.prevButton) {
+    elements.prevButton.addEventListener("click", () => {
+      autoPlayStop();
+      prevSlide();
+    });
+  }
+
+  if (elements.nextButton) {
+    elements.nextButton.addEventListener("click", () => {
+      autoPlayStop();
+      nextSlide();
+    });
+  }
+};
+
+const initializeSlider = () => {
+  updateSlider();
+  sliderNavigation();
+  autoPlayStart();
+};
+
+initializeSlider();
 // #endregion
 
 // #region: Animations
 
 const buttonBreathingAnimation = () => {
-  buttonBreath = gsap.to('.banner__button', {
-    repeat: -1,
-    yoyo: true,
-    scale: 1.1,
-    duration: 1.5,
-    ease: 'power1.inOut',
-  })
-}
+  if (elements.bannerButton) {
+    buttonBreath = gsap.to(".banner__button", {
+      repeat: -1,
+      yoyo: true,
+      scale: 1.1,
+      duration: 1.5,
+      ease: "power1.inOut",
+    });
+  }
+};
 
 const bannerLogoTL = gsap.timeline({ defaults: { duration: 1.5 } });
 
 // Samsung logo animation
 bannerLogoTL
-  .from('.banner__logo', {
+  .from(".banner__logo", {
     x: -300,
   })
-  .to('.banner__logo', {
+  .to(".banner__logo", {
     y: -100,
   });
 
-
 // #region EventListeners
-elements.bannerButton.addEventListener('mouseenter', () => {
+elements.bannerButton.addEventListener("mouseenter", () => {
   buttonBreath.pause();
   gsap.to(sliderConfig.selectors.bannerButton, {
     scale: 1.1,
-    duration: .5,
-  })
+    duration: 0.5,
+  });
 });
 
-elements.bannerButton.addEventListener('mouseleave', () => {
+elements.bannerButton.addEventListener("mouseleave", () => {
   buttonBreath.resume();
 });
 // #endregion
 
-  // Permanent elements animations
+// Permanent elements animations
 
-gsap.from('.banner__special-offer-product', {
+gsap.from(".banner__special-offer-product", {
   x: -300,
   duration: 1.5,
 });
 
-gsap.from('.banner__special-offer-price', {
+gsap.from(".banner__special-offer-price", {
   x: -300,
   duration: 1.5,
 });
 
-gsap.from('.banner__main-text-line', {
+gsap.from(".banner__main-text-line", {
   x: -1000,
   duration: 1.5,
   stagger: 0.5,
   delay: 1,
 });
 
-gsap.to('.banner__main-image', {
-  width: '0',
+gsap.to(".banner__main-image", {
+  width: "0",
   duration: 1,
   delay: 3,
 });
 
-gsap.to('.banner__white-space', {
-  width: '55%', 
+gsap.to(".banner__white-space", {
+  width: "55%",
   delay: 3,
   duration: 1,
 });
 
 // Slider animations
 
-gsap.to('.banner__slider-image', {
-  width: '45%',
+gsap.to(".banner__slider-image", {
+  width: "45%",
   delay: 3,
   duration: 1,
 });
 
-gsap.to('.banner__special-offer-product', {
-  display: 'none',
+gsap.to(".banner__special-offer-product", {
+  opacity: 0,
+  visibility: 'hidden',
   duration: 0.1,
-  delay: 3, 
+  delay: 3,
 });
 
-gsap.to('.banner__special-offer-price', {
-  display: 'none',
+gsap.to(".banner__special-offer-price", {
+  opacity: 0,
+  visibility: 'hidden',
   duration: 0.1,
   delay: 3,
 });
 
 // Note animation
-const noteTL = gsap.timeline({defaults: {opacity: 1}});
+const noteTL = gsap.timeline({ defaults: { opacity: 1 } });
 
-noteTL.to('.banner__note', {
-  opacity: 0,
-  delay: 3,
-  duration: 0.5,
-})
-.to('.banner__note', {
-  opacity: 1,
-  duration: 1,
-  delay: 1,
-});
+noteTL
+  .to(".banner__note", {
+    opacity: 0,
+    delay: 3,
+    duration: 0.5,
+  })
+  .to(".banner__note", {
+    opacity: 1,
+    duration: 1,
+    delay: 1,
+  });
 
-// breath button animation
+// breath premanent elements visibility
 
 gsap.to(sliderConfig.selectors.bannerButton, {
-  display: 'block', 
+  display: "block",
   opacity: 1,
   duration: 1,
   delay: 3,
   onComplete: () => buttonBreathingAnimation(),
-})
+});
+
+gsap.to('.banner__slider-text', {
+  opacity: 1,
+  duration: 1,
+  delay: 3,
+});
+
+gsap.to(sliderConfig.selectors.counter, {
+  opacity: 1,
+  duration: 1,
+  delay: 3,
+});
 // #endregion
